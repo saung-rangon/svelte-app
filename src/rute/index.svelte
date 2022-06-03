71f69d6b4ebe45023c8c45d147d1180412314aac
insan-almunawir <105698404+insan-almunawir@users.noreply.github.com>
2022-06-04 01:50:28 +0700

    Update and rename src/routes/index.svelte to src/rute/index.svelte

--- src/rute/index.svelte
+++ src/rute/index.svelte
@@ -3,8 +3,8 @@
 let tanggal = '';
  
  function runDay() {
-  const time = new Date()
-  tanggal = time.getUTCDay() +','+ time.getDate() +'-'+ time.getMonth() +'-'+ time.getFullYear()
+  const today = new Date()
+  tanggal = today.getUTCDay() +','+ today.getDate() +'-'+ today.getMonth() +'-'+ today.getFullYear()
   setTimeout ( runDay , 24 * 60 * 60 * 1000 )
 }
  
