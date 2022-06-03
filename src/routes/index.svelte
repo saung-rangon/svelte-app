66de3f9c22fb29fc11f3fc514af8efd581223678
insan-almunawir <105698404+insan-almunawir@users.noreply.github.com>
2022-06-04 00:52:53 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script> 
  // gjjj
-
+let tanggal = '';
+ 
+ function runDay() {
+  const time = new Date()
+  tanggal = time.getDay() +','+ time.getDate() +'-'+ time.getMonth() +'-'+ time.getYear()
+  serTimeout(runDay, 24*60*60*1000)
+}
+ 
+ runDay()
+ 
 let jam = '';
  
 function runTimer() {
@@ -11,6 +20,6 @@ function runTimer() {
  
 runTimer()
 </script>
-
+<h1> {tanggal} </h1>
 <h1>Jam: {jam}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
